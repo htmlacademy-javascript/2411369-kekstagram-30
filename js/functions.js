9dd0bf2f98170fbc947d7c077032da8a2597f602
Артур <artur.ayup@gmail.com>
2023-10-23 22:23:39 +0400

    Функция для проверки продолжительности встреч

--- js/functions.js
+++ js/functions.js
@@ -19,3 +19,25 @@ function isPalindrome(string) {
 
 isPalindrome('Лёша на полке клопа нашёл ');
 
+// Функция для подсчета продолжительности встреч в рабочее время
+const convertTime = (allTime) => {
+  const TIME = allTime.split(':');
+  const MINUTES = parseInt(TIME[0], 10) * 60 + parseInt(TIME[1], 10);
+
+  return MINUTES;
+};
+
+const checkTime = (startTime, endTime, meetingStart, meetingDuration) => {
+  const startDayMinutes = convertTime(startTime);
+  const endDayMinutes = convertTime(endTime);
+  const startMeetingMinutes = convertTime(meetingStart);
+
+  return (startDayMinutes <= startMeetingMinutes) && ((startMeetingMinutes + meetingDuration) <= endDayMinutes);
+};
+
+console.log(checkTime('08:00', '17:30', '14:00', 90));
+console.log(checkTime('8:0', '10:0', '8:0', 120));
+console.log(checkTime('08:00', '14:30', '14:00', 90));
+console.log(checkTime('14:00', '17:30', '08:0', 90));
+console.log(checkTime('8:00', '17:30', '08:00', 900));
+
