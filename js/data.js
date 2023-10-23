5a0cb8c2b4c73a3b0f6bba14613c91a479e7e1ef
Артур <artur.ayup@gmail.com>
2023-10-23 21:31:44 +0400

    Модуль, который создает данные

---
+++ js/data.js
@@ -0,0 +1,97 @@
+import {getRandomInteger, getRandomArrayElement, createIdGenerator} from './util.js';
+
+const PICTURE_COUNT = 25;
+const AVATAR_COUNT = 6;
+const LIKE_MIN_COUNT = 15;
+const LIKE_MAX_COUNT = 200;
+const COMMENT_COUNT = 20;
+
+const DESCRIPTIONS = [
+  'Первое описание фотографии',
+  'Второе описание фотографии',
+  'Третье описание фотографии',
+  'Четвертое описание фотографии',
+  'Пятое описание фотографии',
+  'Шестое описание фотографии',
+  'Седьмое описание фотографии',
+  'Восьмое описание фотографии',
+  'Девятое описание фотографии',
+  'Ииии последнее описание фотографии',
+];
+
+const COMMENT_LINES = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
+];
+
+const NAMES = [
+  'Руслан',
+  'Макар',
+  'Александр',
+  'Андрей',
+  'Роман',
+  'Ольга',
+  'Наталья',
+  'Кристина',
+  'Екатерина',
+  'Валерия',
+];
+
+// const getRandomInteger = (min, max) => {
+//   const lower = Math.ceil(Math.min(min, max));
+//   const upper = Math.floor(Math.max(min, max));
+//   const result = Math.random() * (upper - lower + 1) + lower;
+//   return Math.floor(result);
+// };
+
+// const getRandomArrayElement = (elements) =>
+//   elements[getRandomInteger(0, elements.length - 1)];
+
+// const createIdGenerator = () => {
+//   let lastGeneratedId = 0;
+
+//   return () => {
+//     lastGeneratedId += 1;
+//     return lastGeneratedId;
+//   };
+// };
+
+const generateCommentId = createIdGenerator();
+
+const createMessage = () => Array.from(
+  { length: getRandomInteger(1, 2) },
+  () => getRandomArrayElement(COMMENT_LINES)
+).join(' ');
+
+const createComment = () => ({
+  id: generateCommentId(),
+  avatar: `img/avatar-${getRandomInteger(1, AVATAR_COUNT)}.svg`,
+  message: createMessage(),
+  name: getRandomArrayElement(NAMES)
+});
+
+const createPicture = (index) => ({
+  id: index,
+  url: `photos/${index}.jpg`,
+  description: getRandomArrayElement(DESCRIPTIONS),
+  likes: getRandomInteger(LIKE_MIN_COUNT, LIKE_MAX_COUNT),
+  comment: Array.from(
+    { length: getRandomInteger(0, COMMENT_COUNT) },
+    createComment
+  )
+});
+
+const getPictures = () => Array.from(
+  { length: PICTURE_COUNT },
+  (_, pictureIndex) => createPicture(pictureIndex + 1)
+);
+
+getPictures();
+
+export {getPictures};
+
+// console.log(getPictures());
