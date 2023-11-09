aced6f5d7761fcb3dfce2045e448e507b9137ce7
Артур <artur.ayup@gmail.com>
2023-11-09 14:13:05 +0400

    Подключена библиотека NoUiSlider

--- js/form.js
+++ js/form.js
@@ -1,3 +1,6 @@
+import {resetScale} from './scale.js';
+import {init as initEffect, reset as resetEffect} from './effect.js';
+
 const MAX_HASHTAG_COUNT = 5;
 const VALID_SYMBOLS = /^[a-zа-яё0-9]{1,19}$/i;
 const ErrorText = {
@@ -28,6 +31,8 @@ const showModal = () => {
 
 const hideModal = () => {
   form.reset();
+  resetScale();
+  resetEffect();
   pristine.reset();
   overlay.classList.add('hidden');
   body.classList.remove('modal-open');
@@ -97,3 +102,4 @@ pristine.addValidator(
 fileField.addEventListener('change', onFileInputChange);
 cancelButton.addEventListener('click', onCancelButtonClick);
 form.addEventListener('submit', onFormSubmit);
+initEffect();
