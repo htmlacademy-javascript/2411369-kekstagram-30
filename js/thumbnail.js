fae7bb48689942df36497814e5bc5845a9bff855
Артур <artur.ayup@gmail.com>
2023-11-14 12:16:45 +0400

    Мелкие исправления

--- js/thumbnail.js
+++ js/thumbnail.js
@@ -2,13 +2,13 @@ const thumbnailTemplate = document.querySelector('#picture')
   .content
   .querySelector('.picture');
 
-const createThumbnail = ({url, description, likes, comment, id}) => {
+const createThumbnail = ({url, description, likes, comments, id}) => {
   const thumbnail = thumbnailTemplate.cloneNode(true);
 
   thumbnail.querySelector('.picture__img').src = url;
   thumbnail.querySelector('.picture__img').alt = description;
   thumbnail.querySelector('.picture__likes').textContent = likes;
-  thumbnail.querySelector('.picture__comments').textContent = comment.length;
+  thumbnail.querySelector('.picture__comments').textContent = comments.length;
   thumbnail.dataset.thumbnailId = id;
 
   return thumbnail;
